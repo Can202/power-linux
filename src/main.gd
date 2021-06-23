66c5e3f291df622a0395becf61d8f484d6c5b2d9
Can202 <mgoopazo@gmail.com>
2021-06-23 17:00:54 -0400

    upload

---
+++ src/main.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	OS.center_window()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_reboot_pressed():
+	OS.execute("xterm", ["-e", "sudo reboot"])
+
+
+func _on_shutdown_pressed():
+	OS.execute("xterm", ["-e", "sudo poweroff"])
+
+
+
+func _on_back_pressed():
+	get_tree().quit()
